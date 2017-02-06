3e5bbfa171d2b3ed341a76099832a41d47e0c713
newdeveloper15 <14024020023@umt.edu.pk>
2017-02-06 17:27:18 +0500

    AltBeacon library added and Tranmitter and Receiver Code added !

--- app/src/main/java/com/example/raheelkhan/indoorpositioningsystem/MainActivity.java
+++ app/src/main/java/com/example/raheelkhan/indoorpositioningsystem/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.raheelkhan.indoorpositioningsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.RemoteException;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.service.RangedBeacon;
+import org.altbeacon.beacon.service.RunningAverageRssiFilter;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+
+public class MainActivity extends Activity implements BeaconConsumer {
+
+    protected static final String TAG = "MonitoringActivity";
+    private BeaconManager beaconManager;
+//    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Beacon Transmitter Code
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon);
+
+        //Beacon Receiver Code
+        BeaconManager.setRssiFilterImplClass(RunningAverageRssiFilter.class);
+        //RunningAverageRssiFilter.setSampleExpirationMilliseconds(5000l);
+        RangedBeacon.setSampleExpirationMilliseconds(1000);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "BEACON=== " + beacons.iterator().next().getDistance() + ". UUID = " + beacons.iterator().next().getId1());
+                }
+            }
+        });
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
